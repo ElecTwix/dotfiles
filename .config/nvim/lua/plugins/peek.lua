d252640ab53feddd1b5b0f89d097f7d5e1a7ea43
ElecTwix <root@electwix.dev>
2024-02-09 22:41:12 +0300

    nvim

---
+++ .config/nvim/lua/plugins/peek.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "toppair/peek.nvim",
+    event = { "VeryLazy" },
+    build = "deno task --quiet build:fast",
+    config = function()
+      require("peek").setup()
+      -- refer to `configuration to change defaults`
+      vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+      vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+    end,
+  },
+}
