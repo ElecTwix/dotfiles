ce8ac29513436de17db00ac8bb6d7fd3379fbc7e
ElecTwix <root@electwix.dev>
2025-04-23 14:25:07 +0300

    feat(config): update Hyprland and Neovim configurations
    - Adjusted Hyprland render settings for explicit sync and added
      direct_scanout option.
    - Changed desired tab size to 4 in Neovim C filetype configuration.
    - Added SQL filetype configuration with tab size set to 4.
    - Updated lazy-lock.json with new plugin versions and added
      FixCursorHold.nvim.
    - Refactored Neotest configuration to simplify setup and added
      dependencies.
    - Removed redundant Neotest configuration file.

--- .config/nvim/lua/electwix/lazy/core.lua
+++ .config/nvim/lua/electwix/lazy/core.lua
@@ -1,96 +1,23 @@
 return {
 	{
 		"nvim-neotest/neotest",
-		dependencies = { "nvim-neotest/nvim-nio" },
-		opts = {
-			-- Can be a list of adapters like what neotest expects,
-			-- or a list of adapter names,
-			-- or a table of adapter names, mapped to adapter configs.
-			-- The adapter will then be automatically loaded with the config.
-			--[[
-			adapters = {
-				["neotest-golang"] = {
-					go_test_args = { "-v", "-race", "-count=1", "-timeout=1s" },
-					dap_go_enabled = true,
-				},
-			},
-			]]
-			--
-			status = { virtual_text = true },
-			output = { open_on_run = false },
+		dependencies = {
+			"nvim-neotest/nvim-nio",
+			"nvim-lua/plenary.nvim",
+			"antoinemadec/FixCursorHold.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			{ "fredrikaverpil/neotest-golang", version = "*" },
 		},
 		config = function(_, opts)
-			local neotest_ns = vim.api.nvim_create_namespace("neotest")
-			vim.diagnostic.config({
-				virtual_text = {
-					format = function(diagnostic)
-						-- Replace newline and tab characters with space for more compact diagnostics
-						local message =
-							diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
-						return message
-					end,
+			local neotest_golang_opts = {} -- Specify custom configuration
+			require("neotest").setup({
+				adapters = {
+					require("neotest-golang")(neotest_golang_opts), -- Registration
 				},
-			}, neotest_ns)
-
-			opts.consumers = {}
-			-- Refresh and auto close trouble after running tests
-			---@type neotest.Consumer
-			opts.consumers.trouble = function(client)
-				client.listeners.results = function(adapter_id, results, partial)
-					if partial then
-						return
-					end
-					local tree = assert(client:get_position(nil, { adapter = adapter_id }))
-
-					local failed = 0
-					for pos_id, result in pairs(results) do
-						if result.status == "failed" and tree:get_key(pos_id) then
-							failed = failed + 1
-						end
-					end
-					vim.schedule(function()
-						local trouble = require("trouble")
-						if trouble.is_open() then
-							trouble.refresh()
-							if failed == 0 then
-								trouble.close()
-							end
-						end
-					end)
-					return {}
-				end
-			end
-
-			if opts.adapters then
-				local adapters = {}
-				for name, config in pairs(opts.adapters or {}) do
-					if type(name) == "number" then
-						if type(config) == "string" then
-							config = require(config)
-						end
-						adapters[#adapters + 1] = config
-					elseif config ~= false then
-						local adapter = require(name)
-						if type(config) == "table" and not vim.tbl_isempty(config) then
-							local meta = getmetatable(adapter)
-							if adapter.setup then
-								adapter.setup(config)
-							elseif adapter.adapter then
-								adapter.adapter(config)
-								adapter = adapter.adapter
-							elseif meta and meta.__call then
-								adapter = adapter(config)
-							else
-								error("Adapter " .. name .. " does not support setup")
-							end
-						end
-						adapters[#adapters + 1] = adapter
-					end
-				end
-				opts.adapters = adapters
-			end
-
-			require("neotest").setup(opts)
+				quickfix = {
+					enabled = false,
+				},
+			})
 		end,
     -- stylua: ignore
     keys = {
@@ -108,7 +35,6 @@ return {
 	},
 	{
 		"mfussenegger/nvim-dap",
-		optional = true,
     -- stylua: ignore
     keys = {
       { "<leader>td", function() require("neotest").run.run({strategy = "dap"}) end, desc = "Debug Nearest" },
