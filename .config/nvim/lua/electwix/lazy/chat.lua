22b9b1c91fb603d7c01166cdc2d77f7aff3b26fa
ElecTwix <root@electwix.dev>
2025-04-25 00:54:34 +0300

    feat(nvim/chat): add predefined prompts for chat commands

--- .config/nvim/lua/electwix/lazy/chat.lua
+++ .config/nvim/lua/electwix/lazy/chat.lua
@@ -13,6 +13,33 @@ return {
 				window = {
 					width = 0.4,
 				},
+				prompts = {
+					Explain = {
+						prompt = "Write an explanation for the selected code as paragraphs of text.",
+						system_prompt = "COPILOT_EXPLAIN",
+					},
+					Review = {
+						prompt = "Review the selected code.",
+						system_prompt = "COPILOT_REVIEW",
+					},
+					Fix = {
+						prompt = "There is a problem in this code. Identify the issues and rewrite the code with fixes. Explain what was wrong and how your changes address the problems.",
+					},
+					Optimize = {
+						prompt = "Optimize the selected code to improve performance and readability. Explain your optimization strategy and the benefits of your changes.",
+					},
+					Docs = {
+						prompt = "Please add documentation comments to the selected code.",
+					},
+					Tests = {
+						prompt = "Please generate tests for my code.",
+					},
+					Commit = {
+						prompt = "Write commit message for the change with commitizen convention. Keep the title under 50 characters and wrap message at 72 characters. Format as a gitcommit code block. with like feat({package ( such as server )}/{module name ( such as suser )}: Title  \
+					More Info ...	",
+						context = "git:staged",
+					},
+				},
 			}
 		end,
 		keys = {
